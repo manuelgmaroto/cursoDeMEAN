9d04bb1c3c2f16f5e6982d538422bacfab20c76b
manuel gonzalez <manuelgmaroto@gmail.com>
2016-10-18 21:11:37 +0200

    Clase calculadora

--- ejer02/js/calculadora.js
+++ ejer02/js/calculadora.js
@@ -1,6 +1,49 @@
 
-function operacion(arg){
-    console.log(this);
+function Calculadora(){
+    this.resultado;
+    this.suma = function(num){
+        if(this.resultado){
+            this.resultado += num;
+            return this.resultado;
+        }else{
+            this.resultado = num;
+            return this.resultado;
+        }
+    }
+
+    this.resta = function(num){
+        if(this.resultado){
+            this.resultado -= num;
+            return this.resultado;
+        }else{
+            this.resultado = num;
+            return this.resultado;
+        }
+    }
+
+    this.multiplicacion = function(num){
+        if(this.resultado){
+            this.resultado *= num;
+            return this.resultado;
+        }else{
+            this.resultado = num;
+            return this.resultado;
+        }
+    }
+
+    this.division = function(num){
+        if(this.resultado){
+            this.resultado /= num;
+            return this.resultado;
+        }else{
+            this.resultado = num;
+            return this.resultado;
+        }
+    }
+
+    this.borrado = function(){
+        this.resultado = 0;
+    }
 }
 
 //Clase calculadora que le pases los valores e hiciera las operaciones
@@ -9,3 +52,17 @@ function operacion(arg){
 //delete memoria
 
 //nacesito clase intermecia que unieera la calculadora con la vista
+
+var calculadora1 = new Calculadora();
+
+
+console.log(calculadora1.suma(10));
+console.log(calculadora1.suma(10));
+console.log(calculadora1.suma(10));
+console.log(calculadora1.borrado());
+console.log(calculadora1.suma(10));
+console.log(calculadora1.resta(5));
+console.log(calculadora1.borrado());
+console.log(calculadora1.multiplicacion(10));
+console.log(calculadora1.multiplicacion(10));
+console.log(calculadora1.division(2));
