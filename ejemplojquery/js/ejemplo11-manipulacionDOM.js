8b6839d6b9ea2406311be89f24bb30c63f018c39
manana <manana>
2016-10-25 10:26:19 +0200

    Ejemplo 11, manipular DOM

---
+++ ejemplojquery/js/ejemplo11-manipulacionDOM.js
@@ -0,0 +1,53 @@
+$(document).ready(inicializaEventosConJquery);
+
+function inicializaEventosConJquery(){
+   $("#boton1").click(deleteAll);
+   $("#boton2").click(restoreAll);
+   $("#boton3").click(addLastElement);
+   $("#boton4").click(addFirstElement);
+   $("#boton5").click(removeLastElement);
+   $("#boton6").click(deleteFirstElement);
+   $("#boton7").click(deleteFirstAndSecond);
+   $("#boton8").click(deletePreAndLast);
+}
+
+function deleteAll(){
+    $("ul").empty();
+}
+
+function restoreAll(){
+    //Dentro del ul pone esto
+    $("ul").html("<li>Item 1</li><li>Item 2</li><li>Item 3</li><li>Item 4</li>")
+}
+
+function addLastElement(){
+    $("ul").append("<li>Agrego un item al final</li>");
+}
+
+function addFirstElement(){
+    $("ul").prepend("<li>Agrego un item al principio</li>")
+}
+
+function removeLastElement(){
+    let position = $("li").length-1;
+    //eq permite obtener el li que busco
+    let liToDelete = $("li").eq(position);
+    liToDelete.remove();
+}
+
+function deleteFirstElement(){
+    //ahora directamente le ponemos la posicion a eliminar
+    $("li").eq(0).remove();
+}
+
+function deleteFirstAndSecond(){
+    //lt coge los dos primeros elementos de la lista
+    $("li").lt(2).remove();
+}
+
+function deletePreAndLast(){
+    let position = $("li").length-3;
+    let liTodelete = $("li").gt(position);
+    liTodelete.remove();
+}
+
