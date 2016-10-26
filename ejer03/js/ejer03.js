0f4dff75806c9f86718808fa2d9b68b5b1b19f4e
manana <manana>
2016-10-26 09:26:06 +0200

    Botones añadir y borrar peliculas

--- ejer03/js/ejer03.js
+++ ejer03/js/ejer03.js
@@ -1,5 +1,20 @@
 $(document).ready(inicializeEvents);
 
 function inicializeEvents(){
-    
+    $("#botonGuardar").click(addMovie);
+    $("#botonModificar").click(modifyMovie);
+    $("#botonBorrar").click(removeMovie);
+}
+
+function addMovie(){
+    $("#tablaPeliculas").append("<tr><td>Uno</td><td>Dos</td><td>TRes</td><td>Cuatro</td><td>Cinco</td></tr>").fadeIn("slow");
+}
+
+function modifyMovie(){
+
+}
+
+function removeMovie(){
+    let position = $("tr").length-1;
+    $("tr").eq(position).remove();
 }
