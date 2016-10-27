bccbaffe2045ed2bc13b435de8697d8b083ebf42
manana <manana>
2016-10-27 09:50:35 +0200

    Crea página con peliculas del JSON

--- ejer03/js/ejer03.js
+++ ejer03/js/ejer03.js
@@ -28,11 +28,11 @@ function classRemove(){
     $(this).addClass("remove");
     
 }
-
+/*
 function cargarPeliculas(){
     $.get("http://localhost:8080/data/peliculas.json",resultadoGet);
 }
-
+*/
 function peticionAjaxGenerica(){
     //llamamos al metodo de ajax de jquery y le pasamos datos clave valor
     $.ajax({
@@ -44,10 +44,24 @@ function peticionAjaxGenerica(){
         //tipo de dato esperado
         dataType:"json",
         //URL de comunicacion con el servicio
-        url: "https://localhost:8080/data/peliculas.json"
+        url: "http://localhost:3000/peliculas"
     })
     .done(peticionCompletada)
     .fail(peticionFallida);
 }
 
 //Acabar peticion ajax
+
+function peticionCompletada(data,status,jqXHR){
+    //alert("Peticion completada con status " + status + " : " + data);
+    for (let i = 0; i < data.length; i++){
+        $("#tablaPeliculas").append("<tr><td>"+ data[i].id +"</td><td>"+ data[i].titulo +"</td><td>"+ data[i].director +"</td><td>"+ data[i].sinopsis +"</td><td>"+ data[i].fecha +"</td></tr>");
+        $("tr").click(classRemove);
+    }
+}
+
+function peticionFallida(jqXHR,status,error){//el jqXHR es el objeto ajax que lo devuelve
+    console.log("Error al procesar la peticion");
+    console.log("Status " + status);
+    console.log("Error " + error );
+}
