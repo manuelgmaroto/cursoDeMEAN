a79fa6074f6ee1cc2a16824da1614c6630e0f1d1
manana <manana>
2016-10-31 13:52:55 +0100

    Ejemplos typescript

--- ejemplo05-typescript/app.ts
+++ ejemplo05-typescript/app.ts
@@ -23,3 +23,55 @@ function cumplirAnios(fechaDeCumpleanios:Tiempo):void{
     fechaDeCumpleanios.anio++;
 }
 
+
+//Definiciones de clases
+
+class Persona{ 
+    //Definicon de atributos
+    nombre: string;
+    apellido1: string;
+    apellido2: string;
+    // Metodo constructor
+    constructor(nombre:string,apellido1:string,apellido2:string){
+        //Definicion de atributos
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+    }
+    //Metodos de instancia
+    stringify():string{
+        return this.nombre + " " +this.apellido1 + " " + this.apellido2;
+    }
+}
+
+/*Sin metodo constructor
+let manuel = new Persona();
+manuel.nombre ="AMnuel";
+manuel.apellido1 = "Gonzalez";
+manuel.apellido2 = "Maroto";*/
+
+let manuel = new Persona("Manuel","Gonzalez","Maroto");
+let federico = new Persona("Federico","Gomez","Antae");
+console.log(manuel.stringify());
+console.log(federico.stringify());
+
+
+//Extendemos las clases con herencia
+
+class Ciudadano extends Persona {
+    
+    identidad: string;
+
+    constructor(nombre:string,apellido1:string,apellido2:string,identidad:string){
+        super(nombre,apellido1,apellido2);//No copiamos el constructor con super hereda el de persona
+        this.identidad = identidad; //añadimos lo que tiene 
+    }
+    /*Lo sobreescribimos */
+    stringify():string{
+        return super.stringify() + " " + this.identidad;
+    }
+
+}
+
+var ciudadanoPepe = new Ciudadano("Pepe",null,null,"1");
+console.log(ciudadanoPepe.stringify());
--- ejemplo05-typescript/jsScripts/app.js
+++ ejemplo05-typescript/jsScripts/app.js
@@ -1,3 +1,8 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 var variable = 2016;
 //variable = "texto";
 //si en tsconfig ponemos es6 mantiene el let
@@ -11,4 +16,43 @@ console.log("Despues de cumplir años : " + miCumpleanios.anio);
 function cumplirAnios(fechaDeCumpleanios) {
     fechaDeCumpleanios.anio++;
 }
+//Definiciones de clases
+var Persona = (function () {
+    // Metodo constructor
+    function Persona(nombre, apellido1, apellido2) {
+        //Definicion de atributos
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+    }
+    //Metodos de instancia
+    Persona.prototype.stringify = function () {
+        return this.nombre + " " + this.apellido1 + " " + this.apellido2;
+    };
+    return Persona;
+}());
+/*Sin metodo constructor
+let manuel = new Persona();
+manuel.nombre ="AMnuel";
+manuel.apellido1 = "Gonzalez";
+manuel.apellido2 = "Maroto";*/
+var manuel = new Persona("Manuel", "Gonzalez", "Maroto");
+var federico = new Persona("Federico", "Gomez", "Antae");
+console.log(manuel.stringify());
+console.log(federico.stringify());
+//Extendemos las clases con herencia
+var Ciudadano = (function (_super) {
+    __extends(Ciudadano, _super);
+    function Ciudadano(nombre, apellido1, apellido2, identidad) {
+        _super.call(this, nombre, apellido1, apellido2); //No copiamos el constructor con super hereda el de persona
+        this.identidad = identidad; //añadimos lo que tiene 
+    }
+    /*Lo sobreescribimos */
+    Ciudadano.prototype.stringify = function () {
+        return _super.prototype.stringify.call(this) + " " + this.identidad;
+    };
+    return Ciudadano;
+}(Persona));
+var ciudadanoPepe = new Ciudadano("Pepe", null, null, "1");
+console.log(ciudadanoPepe.stringify());
 //# sourceMappingURL=app.js.map
