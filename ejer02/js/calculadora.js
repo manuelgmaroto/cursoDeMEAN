848e80752d8da1860a4a2a52d29d86381421f1dc
manuel gonzalez <manuelgmaroto@gmail.com>
2016-11-01 20:24:58 +0100

    Funciona calculadora desde la vista

--- ejer02/js/calculadora.js
+++ ejer02/js/calculadora.js
@@ -9,21 +9,20 @@ Calculadora.prototype.calcular = function(arg){
     return eval(arg);
 }
 
-/*Calculadora.prototype.sumar = function(arg1){
-    this.resultado = 0;
-    for(let i = 0; i < arguments.length; i++){
-        this.resultado += arguments[i];
-    }
-    return this.resultado;
-
-}*/
-
-
-
 Calculadora.prototype.operacion = function(num,operacion){
     
-   
-   
+    num = parseInt(num);
+
+    if(this.memoria==0){
+        this.memoria = num;
+    }else if(operacion=="+"){
+        this.memoria += num;
+    }else if(operacion == "-"){
+        this.memoria -= num;
+    }
+    return this.memoria;
+
+   /*
     switch(operacion){
     case "+":
         this.memoria+=num;
@@ -36,44 +35,15 @@ Calculadora.prototype.operacion = function(num,operacion){
     }
     
     return this.memoria;
+    */
 }
 
-var calculadora3 = new Calculadora();
-
-
-console.log(calculadora3.operacion(16));
-console.log(calculadora3.operacion(32,"-"));
+//TODO no borra bien
 
 
 
 
 
-//TODO:funcion que verifique que lo que entra es un number
-
-/*
-var calculadora2 = new Calculadora();
-
-console.log(calculadora2.sumar(10,20,40));
-console.log(calculadora2.sumar(10));
-
-//Clase calculadora que le pases los valores e hiciera las operaciones
-//un valor lo acumula
-//operar(operador,valor) cada vez manda el resultado
-//delete memoria
-
-//nacesito clase intermecia que unieera la calculadora con la vista
-/*
-var calculadora1 = new Calculadora();
 
 
-console.log(calculadora1.suma(10));
-console.log(calculadora1.suma(10));
-console.log(calculadora1.suma(10));
-console.log(calculadora1.borrado());
-console.log(calculadora1.suma(10));
-console.log(calculadora1.resta(5));
-console.log(calculadora1.borrado());
-console.log(calculadora1.multiplicacion(10));
-console.log(calculadora1.multiplicacion(10));
-console.log(calculadora1.division(2));
-*/
+
--- ejer02/js/controlador.js
+++ ejer02/js/controlador.js
@@ -1,6 +1,6 @@
 window.addEventListener('load',iniciar);
 //inicializo las variables para crear la calculadora, para la operacion, el display y el resultado
-var calculadora1, numero, display,resultado;
+var calculadora1, numero, display,operador;
 
 //funcion que inicia la calculadora cuando el documento está completamente cargado, asigna un 0 en el
 // display y lo escribe, inicializa las teclas para agregarles un evento 
@@ -18,10 +18,13 @@ function iniciar(){
     for(let i = 0; i < teclas.length; i++){
         teclas[i].addEventListener('click',crearNumero);
     }
+    for(let i = 0;i < teclasCalcular.length; i++){
+        teclasCalcular[i].addEventListener('click',calcular);
+    }
     
     teclaBorrar.addEventListener('click',borrar);
-    teclaIgual.addEventListener('click',calcular);
-    teclasCalcular.addEventListener('click',calcular);
+    teclaIgual.addEventListener('click',darResultado);
+    
 }
 
 function crearNumero(){
@@ -40,15 +43,25 @@ function calcular(){
     display.value = resultado;
     operacion = resultado;
     */
+ 
     display.value = calculadora1.operacion(numero,this.value);
+    numero = 0;
+    operador = this.value;
+
+}
+//Funcion que toma los ultimos valores dados de numero y operacion para calcular el resultado al pulsar el igual
+function darResultado(){
+    var valorEnviar = display.value;
+    display.value = calculadora1.operacion(valorEnviar,operador);
 }
 
 function borrar(){
     display.value = 0;
+    numero =0;
     calculadora1.operacion(0);
 }
 
-    
+
 
     
 
